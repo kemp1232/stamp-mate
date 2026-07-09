5bed376f2d6ea11b9b5f59a8f2971da2ed7b83e2
kemp <kempsayson@firstmate.tech>
2026-07-09 10:57:36 +0800

    log register

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -33,6 +33,7 @@ export async function registerOwner(
     });
     userId = user.id;
   } catch (err) {
+    console.log("err", err);
     if (err instanceof APIError) {
       return { error: err.message };
     }
